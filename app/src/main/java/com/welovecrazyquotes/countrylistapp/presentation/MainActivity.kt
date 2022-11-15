b6f121f1abae546f29beea11984f59cc52d028f6
ericwafula <ericwathome007@gmail.com>
2022-11-15 20:35:54 +0300

    migrated nightMode configuration to toolbar menu

--- app/src/main/java/com/welovecrazyquotes/countrylistapp/presentation/MainActivity.kt
+++ app/src/main/java/com/welovecrazyquotes/countrylistapp/presentation/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.welovecrazyquotes.countrylistapp.presentation
 
-import android.content.SharedPreferences
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.welovecrazyquotes.countrylistapp.presentation.adapter.CountryAdapter
 import com.welovecrazyquotes.countrylistapp.R
 import com.welovecrazyquotes.countrylistapp.common.SettingsDataStore
 import com.welovecrazyquotes.countrylistapp.databinding.ActivityMainBinding
@@ -28,29 +27,63 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
 
+    companion object {
+        private val TAG = this::class.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-        navController = navHostFrag.navController
+        setSupportActionBar(binding.toolbar)
 
-//        setupActionBarWithNavController(navController)
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController = navHostFrag.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         settingsDataStore = SettingsDataStore(this)
-        darkMode()
+
+        enableDarkMode()
     }
 
-    private fun darkMode() {
-        enableDarkMode()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
 
-        binding.uiMode.setOnClickListener {
-            enableDarkMode = !enableDarkMode
-            viewModel.enableDarkMode(enableDarkMode)
-            lifecycleScope.launchWhenCreated {
-                settingsDataStore.saveDarkModeToPreferences(enableDarkMode, this@MainActivity)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.ui_mode -> {
+                enableDarkMode = !enableDarkMode
+                viewModel.enableDarkMode(enableDarkMode)
+                lifecycleScope.launchWhenCreated {
+                    viewModel.isDarkModeEnabled.collect {
+                        settingsDataStore.saveDarkModeToPreferences(it, this@MainActivity)
+                    }
+                }
+                invalidateOptionsMenu()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val menuItem = menu?.findItem(R.id.ui_mode)
+        lifecycleScope.launchWhenCreated {
+            viewModel.isDarkModeEnabled.collect { isEnabled ->
+                menuItem?.let {
+                    if (isEnabled) {
+                        menuItem.icon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.sun)
+                    } else {
+                        menuItem.icon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.moon)
+                    }
+                }
             }
         }
+        return super.onPrepareOptionsMenu(menu)
     }
 
     private fun enableDarkMode() {
@@ -58,12 +91,8 @@ class MainActivity : AppCompatActivity() {
             settingsDataStore.dataStore.collect { isDarkModeEnabled ->
                 if (isDarkModeEnabled) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    binding.uiMode.setImageResource(R.drawable.sun)
-                    binding.title.setTextColor(Color.WHITE)
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    binding.uiMode.setImageResource(R.drawable.moon)
-                    binding.title.setTextColor(Color.BLACK)
                 }
             }
             /**
